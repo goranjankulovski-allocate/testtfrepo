c3cf374850b7e9cf27d2c4a74f990cfb1e85119c
Goran Jankulovski <99654269+goranjankulovski-allocate@users.noreply.github.com>
2024-12-13 15:15:10 +0100

    Add providers.tf

---
+++ testproduct/preprod/eu-west-1/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  allowed_account_ids = var.allowed_aws_account_ids
+  assume_role {
+    role_arn = var.assume_role_arn
+  }
+  region = "eu-west-1"
+  default_tags {
+    tags = local.tags
+  }
+}
+
