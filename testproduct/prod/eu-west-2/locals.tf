5f327bfdd274ddb4ad1ab523bff10b354ff02054
Goran Jankulovski <99654269+goranjankulovski-allocate@users.noreply.github.com>
2024-12-13 15:15:14 +0100

    Add locals.tf

---
+++ testproduct/prod/eu-west-2/locals.tf
@@ -0,0 +1,20 @@
+locals {
+  name = "testtfgit"
+  region = "eu-west-2"
+  env = "prod"
+  product = "testproduct"
+  tags {
+    "product"              = "testproduct"
+    "product-family"       = "internal-shared-services"
+    "product-group"        = "shared-hosting"
+    "env"                  = "prod"
+    "geo"                  = "uk"
+    "client"               = "shared"
+    "business-unit"        = "uki-health"
+    "department"           = "development-engineering"
+    "ManagedBy"            = "Terraform"
+    "terraform-repository" = "https://github.com/rld-devops/platform-infrastructure.git"
+    "terraform-workspace"  = "platform-uk-test"
+    "debtor-id"            = "ALL001"
+  }
+}
