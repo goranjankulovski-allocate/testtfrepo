6ea7301b6f3c3a0d68c7fa0a8eb87d61ee3f973c
Goran Jankulovski <99654269+goranjankulovski-allocate@users.noreply.github.com>
2024-12-16 14:27:36 +0100

    Add providers.tf

---
+++ productname/us-preprod/providers.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.15.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.32.0"
+    }
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = "~> 2.0"
+    }
+    mongodbatlas = {
+      source  = "mongodb/mongodbatlas"
+      version = "~> 1.18.0"
+    }
+    datadog = {
+      source  = "datadog/datadog"
+      version = "~> 3.43"
+    }
+  }
+}
+
+provider "aws" {
+  allowed_account_ids = ["418295682908"]
+  assume_role {
+    role_arn = "arn:aws:iam::418295682908:role/aft-terraform-runner"
+  }
+  region = local.region
+  default_tags {
+    tags = local.tags
+  }
+}
+
