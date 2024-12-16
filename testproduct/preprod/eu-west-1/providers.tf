9222fcde59b3ccb61a367d0bfe8368b67ceffef1
Goran Jankulovski <99654269+goranjankulovski-allocate@users.noreply.github.com>
2024-12-16 11:25:49 +0100

    Add providers.tf

--- testproduct/preprod/eu-west-1/providers.tf
+++ testproduct/preprod/eu-west-1/providers.tf
@@ -1,8 +1,28 @@
-terraform {
+  terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.15.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.32.0"
+    }
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = "~> 2.0"
+    }
+    mongodbatlas = {
+      source  = "mongodb/mongodbatlas"
+      version = "~> 1.18.0"
+    }
+    datadog = {
+      source  = "datadog/datadog"
+      version = "~> 3.43"
     }
   }
 }
