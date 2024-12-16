4ffa92d06cf06de48f1633c7278218610260fa40
Goran Jankulovski <99654269+goranjankulovski-allocate@users.noreply.github.com>
2024-12-16 14:27:48 +0100

    Add locals.tf

---
+++ productname/us-preprod/locals.tf
@@ -0,0 +1,19 @@
+locals {
+  region = "us-east-2"
+  env = "preprod"
+  product = "productname"
+  tags = {
+    "product"              = "productname"
+    "product-family"       = "internal-shared-services"
+    "product-group"        = "shared-hosting"
+    "env"                  = "preprod"
+    "geo"                  = "uk"
+    "client"               = "shared"
+    "business-unit"        = "uki-health"
+    "department"           = "development-engineering"
+    "ManagedBy"            = "Terraform"
+    "terraform-repository" = "https://github.com/rld-devops/platform-infrastructure.git"
+    "terraform-workspace"  = "platform-uk-test"
+    "debtor-id"            = "ALL001"
+  }
+}
