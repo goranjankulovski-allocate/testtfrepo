badca3e7c66484f616e87ea9ce1e8440fbffd1e1
Goran Jankulovski <99654269+goranjankulovski-allocate@users.noreply.github.com>
2024-12-16 14:29:07 +0100

    Add backend.tf

---
+++ productname/us-dev/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+    backend "remote" {
+        hostname     = "app.terraform.io"
+        organization = "allocatesoftware"
+        workspaces {
+            name = "platform-us-production-productname-dev"
+        }
+    }
+}
